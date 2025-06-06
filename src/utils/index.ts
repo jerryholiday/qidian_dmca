13ffb5e692067b45aa08e1e66101e564f944e58d
p_leiai <p_leiai@tencent.com>
2025-06-06 17:09:56 +0800

    fix: createHash

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
-import crypto from 'crypto';
+import { createHash } from 'crypto';
 
 export const md5 = (str: string) => {
-  const hash = crypto.createHash('md5');
+  const hash = createHash('md5');
   hash.update(str);
   return hash.digest('hex');
 };
